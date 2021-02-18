8e0d7efe33fa70a7508e4af54b28d965eeea5d5f
Kyle McKay <kyle.mckay@live.com>
2021-02-18 14:44:45 -0700

    Update WAF_BLOCK_LOGGING.tcl

--- WAF_BLOCK_LOGGING.tcl
+++ WAF_BLOCK_LOGGING.tcl
@@ -8,7 +8,6 @@ when ASM_REQUEST_DONE
                 set deviceid [URI::decode $DID]
                 set deviceida [lindex [regexp -inline -- (?:"diA":")(.*?)(?:") $deviceid] 1]
                 set deviceidb [lindex [regexp -inline -- (?:"diB":")(.*?)(?:") $deviceid] 1]
-                #HTTP::header insert X-ASM "Violation=[lindex $viodata 0] Client IP=[lindex $viodata 4] DID_A=$deviceida DID_B=$deviceidb"
                 log local0.debug "WAF Action: [lindex $viodata 6] -  Violation=[lindex $viodata 0] , Client IP=[lindex $viodata 4] , DID_A=$deviceida , DID_B=$deviceidb"
             }
             else { log local0.debug "No DeviceID found for Violation=[lindex $viodata 0] , Client IP=[lindex $viodata 4] this is either an initial request from the client or the client is not accepting cookies"
